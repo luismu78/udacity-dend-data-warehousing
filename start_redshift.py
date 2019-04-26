416264a5f4dcfa498912cb8ce56952a542c713e1
rhoneybul <honeybulr@gmail.com>
2019-04-26 12:10:00 +0800

    removed security group creation, and updated config example

--- start_redshift.py
+++ start_redshift.py
@@ -77,65 +77,55 @@ def create_iam(config):
     print('Successfully Created Role, and Attached S3 Read-Only Policy.')
     return role
 
-
-def create_security_group(config):
-    """
-    creates the security group such the redshift cluster can be accessed on the 
-    neccessary port, and subsequently authorizes the security group such that 
-    it exposes the required port
-
-    Arguments:
-    config: configuration object, loaded in from dwh.cfg
+def create_redshift_cluster(config, security_group, iam_role):
+    """create redshift cluster
+    initiates the creation of a redshift cluster created based on the 
+    cluster parameters given in the config file 
+    and waits till the cluster is up and running.
+    Parameters:
+    config: config object
+    security_group: security group created for redshift to allow external access
+    iam_role: iam role created for the redshift cluster
     Returns:
-    security_group: security group metadata from the created security group.
+    redshift_cluster: metadata regarding the redshift cluster
     """
 
-    EC2 = boto3.client(
-        'ec2',
+    redshift = boto3.client(
+        'redshift',
         aws_access_key_id=config.get('AWS_ACCESS', 'AWS_ACCESS_KEY_ID'),
         aws_secret_access_key=config.get('AWS_ACCESS', 'AWS_SECRET_ACCESS_KEY'),
-        region_name=config.get('AWS_ACCESS', 'AWS_REGION')
+        region_name=config.get('AWS_ACCESS', 'AWS_REGION'), 
     )
 
-    try:
-        security_group = EC2.create_security_group(
-            Description='Security Group to Allow External Access to Redshift',
-            GroupName=config.get('CLUSTER', 'SECURITY_GROUP_NAME'),
-        )
-    except botocore.exceptions.ClientError as e:
-        print('Handling Create Security Group Err:', e)
-        security_group = EC2.describe_security_groups(
-            GroupNames=[
-                config.get('CLUSTER', 'SECURITY_GROUP_NAME')
-            ]
-        )['SecurityGroups'][0]
-
-    try:
-        EC2.authorize_security_group_ingress(
-            GroupName=config.get('CLUSTER', 'SECURITY_GROUP_NAME'),
-            CidrIp='0.0.0.0/0',
-            IpProtocol='TCP',
-            FromPort=int(config.get('CLUSTER', 'DB_PORT')),
-            ToPort=int(config.get('CLUSTER', 'DB_PORT'))
-        )
-    except botocore.exceptions.ClientError as e: 
-        print('Handling Authorize Security Group Err:', e)
-    
-    print('Security Group Created, and security group authorized:', security_group)
-
+    print('Creating Redshift Cluster...')
+    response = redshift.create_cluster(
+        DBName=config.get('CLUSTER', 'DB_NAME'),
+        ClusterIdentifier=config.get('CLUSTER', 'CLUSTER_IDENTIFIER'),
+        MasterUsername=config.get('CLUSTER', 'DB_USER'),
+        MasterUserPassword=config.get('CLUSTER', 'DB_PASSWORD'),
+        # ClusterSecurityGroups=[
+        #     security_group['GroupName']
+        # ],
+        NodeType=config.get('CLUSTER', 'NODE_TYPE'),
+        Port=int(config.get('CLUSTER', 'DB_PORT')),
+        IamRoles=[
+            iam_role['Role']['Arn']
+        ],
+        NumberOfNodes=int(config.get('CLUSTER', 'NODE_COUNT'))
+    )
+    print('Redshift Cluster Response.', response)
 
 def main():
     """
     - loads in the config
     - creates iam role
-    - creates security group
     - creates redshift cluster
     - waits for redshift cluster to be created, and displays details
     """
 
     config = get_config()
     iam_role = create_iam(config)
-    security_group = create_security_group(config)
+    create_redshift_cluster(config, security_group, iam_role)
 
 
 if __name__ == '__main__':
