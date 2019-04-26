308decadaed8bef7591233815685fff90843b473
rhoneybul <honeybulr@gmail.com>
2019-04-26 14:09:06 +0800

    updated sql querires due to errs

--- create_tables.py
+++ create_tables.py
@@ -19,11 +19,28 @@ def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    HOST = config.get('CLUSTER', 'HOST')
+    DB_NAME = config.get('CLUSTER', 'DB_NAME')
+    DB_USER = config.get('CLUSTER', 'DB_USER')
+    DB_PASSWORD = config.get('CLUSTER', 'DB_PASSWORD')
+    DB_PORT = config.get('CLUSTER', 'DB_PORT')
+    CONNECTION_STRING = "host={} dbname={} user={} password={} port={}".format(
+        HOST,
+        DB_NAME, 
+        DB_USER, 
+        DB_PASSWORD, 
+        DB_PORT,
+    )
+    print('Connecting to RedShift on', CONNECTION_STRING)
+    conn = psycopg2.connect(CONNECTION_STRING)
+    print('Connected to Redshift')
     cur = conn.cursor()
-
+    
+    print('Dropping Tables...')
     drop_tables(cur, conn)
+    print('Dropped Tables, Creating Tables')
     create_tables(cur, conn)
+    print('Created Tables')
 
     conn.close()
 
--- sql_queries.py
+++ sql_queries.py
@@ -19,86 +19,94 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 staging_events_table_create= ("""
 CREATE TABLE IF NOT EXISTS staging_events(
-    artist varchar 
-    auth varchar
-    firstName varchar
-    gender varchar
-    lastName varchar
-    length double precision
-    location varchar
-    page varchar
-    ts integer
-    userAgent varchar
+    artist varchar, 
+    auth varchar,
+    firstName varchar,
+    gender varchar,
+    lastName varchar,
+    length double precision,
+    location varchar,
+    page varchar,
+    ts integer,
+    userAgent varchar,
     user_id varchar
 )
 """)
 
 staging_songs_table_create = ("""
 CREATE TABLE IF NOT EXISTS staging_songs(
-    artist_id varchar 
-    artist_latitude double precision
-    artist_location varchar
-    artist_longitude double precision
-    artist_name varchar
-    duration double precision
-    song_id varchar
-    title varchar
+    artist_id varchar, 
+    artist_latitude double precision,
+    artist_location varchar,
+    artist_longitude double precision,
+    artist_name varchar,
+    duration double precision,
+    song_id varchar,
+    title varchar,
     year integer
 )
 """)
 
 songplay_table_create = ("""
 CREATE TABLE IF NOT EXISTS songplays (
-    songplay_id integer NOT NULL PRIMARY KEY
-    start_time integer SORKEY
-    user_id varchar DISTKEY FOREIGN KEY REFERENCES users(user_id)
-    level varchar 
-    song_id varchar DISTKEY FOREIGN KEY REFERENCES songs(song_id)
-    artist_id varchar FOREIGN KEY REFERENCES artists(artist_id)
-    session_id integer SORTKEY
-    location varchar
-    user_agent varchar
+    songplay_id integer NOT NULL,
+    start_time integer SORTKEY,
+    user_id varchar DISTKEY,
+    level varchar,
+    song_id varchar DISTKEY,
+    artist_id varchar,
+    session_id integer SORTKEY,
+    location varchar,
+    user_agent varchar,
+    PRIMARY KEY (songplay_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (song_id) REFERENCES songs(song_id),
+    FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
 )
 """)
 
 user_table_create = ("""
 CREATE TABLE IF NOT EXISTS users(
-    user_id varchar NOT NULL PRIMARY KEY DISTKEY
-    first_name varchar
-    last_name varchar
-    gender varchar
-    level varchar
+    user_id varchar NOT NULL DISTKEY,
+    first_name varchar,
+    last_name varchar,
+    gender varchar,
+    level varchar,
+    PRIMARY KEY (user_id)
 )
 """)
 
 song_table_create = ("""
 CREATE TABLE IF NOT EXISTS songs(
-    song_id varchar NOT NULL PRIMARY KEY DISTKEY
-    title varchar
-    artist_id varchar FOREIGN KEY REFERENCES artists(artist_id)
-    year integer SORT KEY
-    duration double precision
+    song_id varchar NOT NULL DISTKEY,
+    title varchar,
+    artist_id varchar,
+    year integer SORT KEY,
+    duration double precision,
+    PRIMARY KEY (song_id),
+    FOREIGN KEY artist_id REFERENCES artists(artist_id)
 )
 """)
 
 artist_table_create = ("""
 CREATE TABLE IF NOT EXISTS artists(
-    artist_id varchar NOT NULL PRIMARY KEY
-    name varchar
-    location varchar
-    latitude double precision
-    longitude double precision
+    artist_id varchar NOT NULL,
+    name varchar,
+    location varchar,
+    latitude double precision,
+    longitude double precision,
+    PRIMARY KEY (artist_id)
 )
 """)
 
 time_table_create = ("""
 CREATE TABLE IF NOT EXISTS time(
-    start_time integer NOT NULL
-    hour integer
-    day integer
-    week integer
-    month integer
-    year integer
+    start_time integer NOT NULL,
+    hour integer,
+    day integer,
+    week integer,
+    month integer,
+    year integer,
     weekday integer
 )
 """)
@@ -106,10 +114,18 @@ CREATE TABLE IF NOT EXISTS time(
 # STAGING TABLES
 
 staging_events_copy = ("""
-""").format()
+COPY staging_events
+(artist, auth, firstName, gender, lastName, length, location, page, ts, userAgent, user_id)
+FROM {}
+FORMAT JSON AS {} 
+""").format(config.get('S3', 'LOG_DATA'), config.get('S3', 'LOG_JSONPATH'))
 
 staging_songs_copy = ("""
-""").format()
+COPY staging_songs
+(artist_id, artist_latitude, artist_location, artist_longitude, artist_name, duration, song_id, title, year)
+FROM {}
+FORMAT JSON AS auto
+""").format(config.get('S3', 'SONG_DATA'))
 
 # FINAL TABLES
 
